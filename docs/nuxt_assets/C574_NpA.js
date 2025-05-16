ad842ac4275f06564961fe59faed582bb29278bb
聪聪不是冲冲 <862348941@qq.com>
2025-05-16 16:14:03 +0800

    优化代码

--- docs/nuxt_assets/C574_NpA.js
+++
@@ -1,1 +0,0 @@
-import{E as s}from"./SAaL3YFx.js";const o=e=>`${s().app.baseURL||"/"}images/${e}`;export{o as g};
---
+++ docs/nuxt_assets/D-CTRh6c.js
@@ -0,0 +1,1 @@
+import{E as s}from"./vzowsMVP.js";const o=e=>`${s().app.baseURL||"/"}images/${e}`;export{o as g};
